e92d876c53a1c857e8cb0a6424b44a7094b383d2
Jacob Gasser <jacobgasser17@gmail.com>
2020-09-28 11:12:58 -0500

    launch Updates

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var port = 6969;
 
 app.use( express.static( "public" ) );
 
@@ -32,4 +31,7 @@ app.get('/*', (req,res)=>{
     res.render('ahahah.ejs')
 });
 
-app.listen(port);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
